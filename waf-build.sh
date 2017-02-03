770f008826e5e2bfd16165dcd2bd61b2adfc8e55
Aaron Smith <projects@aaronsmith.co>
2017-02-02 21:24:49 -0800

    adding waf build stuff...

---
+++ waf-build.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+alias waf="$PWD/modules/waf/waf-light"
+sudo ln -s /usr/bin/arm-unknown-linux-gnueabihf-pkg-config /usr/bin/arm-linux-gnueabihf-pkg-config
+sudo chmod a+x /usr/bin/arm-linux-gnueabihf-pkg-config
+waf configure --board=navio
+
+waf ahrs
+waf copter
